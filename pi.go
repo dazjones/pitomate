64fc08a3cba68510a5db5b62160683e999570328
Darren Jones <darren.jones@fields-mail.com>
2016-01-22 16:13:30 +0000

    added message handler

--- pi.go
+++ pi.go
@@ -1,1 +1,17 @@
 package pitomate
+
+import (
+	"fmt"
+	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	"os"
+	"time"
+)
+
+var f MQTT.MessageHandler = func(client *MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+
+}
