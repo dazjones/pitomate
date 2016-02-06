1ca78fda68e1b5caf231d80cc6a486a9d7daa3d1
Darren Jones <darren@darrenjonesdesign.co.uk>
2016-02-06 18:55:08 +0000

    moved database connection so a seperate package for reuse

--- dbtools/dbtools.go
+++ dbtools/dbtools.go
@@ -7,13 +7,22 @@ import (
 	"strings"
 )
 
+var db *sql.DB
+
 func GetConnection() *sql.DB {
+
+	db = *db
+
+	if db != nil {
+		return db
+	}
+
 	cnf := conf.GetConf()
 
 	conStrings := []string{cnf.MySQLUser, ":", cnf.MySQLPass, "@tcp(",
 		cnf.MySQLHost, ":", cnf.MySQLPort ,")/", cnf.MySQLName, "?charset=utf8"}
 
-	db, _ := sql.Open("mysql", strings.Join(conStrings, ""))
+	db, _ = sql.Open("mysql", strings.Join(conStrings, ""))
 
 	return db
 }
